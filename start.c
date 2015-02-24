44d198800aa902152581621217ec846ba607d2bb
finallyjustice <dongli.zhang0129@gmail.com>
2015-02-24 00:27:59 -0500

    add exception class

--- start.c
+++ start.c
@@ -2,21 +2,20 @@
 #include "arm_asm.h"
 
 struct cpu_t vcpu;
+unsigned int reason;
+unsigned int ret_addr;
 
 void do_hvc_handler()
 {
-	//cprintf("hvc_handler!: 0x%x\n", vcpu.lr);
-	printstr("hvc_handler\n");
-	printstr("hvc_handler2\n");
-	//while(1);
+	cprintf("Exception class (0x%x): 0x%x - 0x%x, 0x%x\n", reason>>26, reason, ret_addr, vcpu.lr);
+	while(1);
 }
 
 void hypervisor_main()
 {
-	//cprintf("Hello World!\n");
-	printstr("Nihao\n");
+	cprintf("Hello World!\n");
 	copy_guest();
 	guest_start(0, 2272, 0x80000100);
 
-	while(1);
+	//while(1);
 }
